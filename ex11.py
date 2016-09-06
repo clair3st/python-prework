2e06964b422fe8f57ada9ec689531e6be1d64718
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-09-05 18:24:21 -0700

    completed ex11 asking questions

---
+++ ex11.py
@@ -0,0 +1,22 @@
+# ex11: Asking questions
+
+# comma at the end of the print statements so the print doesn't
+#  end the line with the new line character and go to the next line.
+# raw_input is an inbuilt python function, it reads a line from
+#  input and converst to a string and retunrs that
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+print "What is your favourite color?",
+age = raw_input()
+print "What is your favourite food?",
+food = raw_input()
+print "What is your 2 favourite numbers?",
+number1 = raw_input()
+number2 = raw_input()
