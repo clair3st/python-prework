4617f98af874351418b827eacd808bff6fade69c
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-11-09 18:10:50 -0800

    completed unit8 ex 10

--- unit8.py
+++ unit8.py
@@ -96,4 +96,24 @@ def scrabble_score(word):
                word_score += score[letter]
     return word_score
 
-    
+# TODO: Write a function called censor that takes two strings, text and word, as input. It should return the text with the word you chose replaced with asterisks.
+#
+# For example:
+#
+# censor("this hack is wack hack", "hack")
+# should return
+#
+# "this **** is wack ****"
+# Assume your input strings won't contain punctuation or upper case letters.
+# The number of asterisks you put should correspond to the number of letters in the censored word.
+
+def censor(text, word):
+    list_text = text.split(' ')
+    answer = ''
+    for i in list_text:
+        if i == word:
+            i = '*'*len(word)
+            answer += (i + ' ')
+        else: answer += (i + ' ')
+    answer = answer[:-1]
+    return answer
